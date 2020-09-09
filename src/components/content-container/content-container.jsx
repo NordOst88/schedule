7371dcf483d489d29f97d6ab9ab27119741e342c
zzVIPz <viperexe@mail.ru>
2020-09-09 16:50:22 -0400

    feat: add fetch to store

--- src/components/content-container/content-container.jsx
+++ src/components/content-container/content-container.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Layout } from 'antd';
+import SwaggerService from '../../services/swagger-service';
+import { onSetEvents } from '../../actions/actions';
 
 import './content-container.scss';
 
-const ContentContainer = ({ currentView, role }) => {
-  const { Content } = Layout;
+class ContentContainer extends PureComponent {
+  api = new SwaggerService();
 
-  return (
-    <Content>
-      <p className="text">Current View: {currentView}</p>
-      <p className="text">Current Role: {role}</p>
-    </Content>
-  );
-};
+  componentDidMount() {
+    const { onFetch } = this.props;
+    this.api.getAllEvents().then((events) => onFetch(events));
+  }
+
+  render() {
+    const { currentView, role } = this.props;
+    const { Content } = Layout;
+    return (
+      <Content>
+        <p className="text">Current View: {currentView}</p>
+        <p className="text">Current Role: {role}</p>
+      </Content>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   ...state,
 });
 
-export default connect(mapStateToProps)(ContentContainer);
+export default connect(mapStateToProps, { onFetch: onSetEvents })(ContentContainer);
--- src/index.js
+++ src/index.js
@@ -4,17 +4,10 @@ import { Provider } from 'react-redux';
 
 import App from './components/app/app';
 import ErrorBoundary from './components/error-boundary/error-boundary';
-import SwaggerService from './services/swagger-service';
 
 import store from './store';
 
-const api = new SwaggerService();
-
 // todo: only for extension redux devtool. Delete before deploy
-
-const result = api.getAllEvents();
-
-console.log('+++', result, store.getState());
 
 render(
   <Provider store={store}>
