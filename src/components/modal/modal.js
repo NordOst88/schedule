254ba437f04e150496fcc720418ed1886e2ff817
Paul <tropaul322@gmail.com>
2020-09-10 18:12:45 +0300

    feat: add time and links

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -7,7 +7,18 @@ export default class ModalInfo extends Component {
     visible: false,
     id: 1,
   };
-  getTime() {}
+
+  getTime = (time) => {
+    const date = new Date(+time);
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+    const month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    const year = date.getFullYear();
+    const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+    const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+
+    return <span>{`${year}-${month}-${day} ${hours}:${minutes}`}</span>;
+  };
+
   showModal = () => {
     this.setState({
       visible: true,
@@ -31,21 +42,36 @@ export default class ModalInfo extends Component {
       type,
       dateTime,
       deadline,
+      links,
     } = this.props.data[id];
+
     const names = organizer.map((el) => (
       <div key={Math.random() * 100}>
         <a href={el.url}>{el.name}</a>
       </div>
     ));
-    console.log(new Date(+deadline));
+
     const types = typeof type === 'object' ? type.join(' , ') : <span>{type}</span>;
+
     const deadlineSpan =
       deadline === '' ? null : (
         <span>
           <span className="modal_bold">Deadline: </span>
-          {deadline}
+          {this.getTime(deadline)}
         </span>
       );
+
+    const usefullLinks =
+      links === []
+        ? null
+        : links.map((el) => (
+            <>
+              <div key={Math.random() * 100}>
+                <a href={el.url}>{el.name}</a>
+              </div>
+            </>
+          ));
+
     return (
       <>
         <Button type="primary" onClick={this.showModal}>
@@ -77,13 +103,19 @@ export default class ModalInfo extends Component {
             <span className="modal_bold">TaskUrl: </span>{' '}
             <a href={descriptionUrl}>{descriptionUrl}</a>
           </p>
-          <div>
-            <span className="modal_bold">Organizer:</span> {names}
+          <div className="modal_flex">
+            <div>
+              <span className="modal_bold">Organizer:</span> {names}
+            </div>
+            <div className="modal_flex-ml">
+              <span className="modal_bold">Usefull links:</span> {usefullLinks}
+            </div>
           </div>
           <br />
-          <div className="modal_date">
+          <div className="modal_flex-sb">
             <span>
               <span className="modal_bold">Date: </span>
+              {this.getTime(dateTime)}
             </span>
             {deadlineSpan}
           </div>
