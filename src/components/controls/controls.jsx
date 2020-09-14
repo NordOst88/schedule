88882664784fe4092292abd127d4f410d8b91eba
zzVIPz <viperexe@mail.ru>
2020-09-14 00:36:48 -0400

    feat: add button print

--- src/components/controls/controls.jsx
+++ src/components/controls/controls.jsx
@@ -1,30 +1,56 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { connect } from 'react-redux';
 import { Menu, Button } from 'antd';
-// import { AppstoreTwoTone } from '@ant-design/icons';
+import { PrinterOutlined } from '@ant-design/icons';
+// import jsPDF from 'jspdf';
+// import html2canvas from 'html2canvas';
 import OptionPicker from '../option-picker/option-picker';
 import { VIEW_MODES } from '../../constants/constants';
 import TIMEZONE from '../../constants/timezone';
 import { onViewModeChange, onTimezoneChange } from '../../actions/actions';
+import print from '../../utils/print';
 
 import './controls.scss';
 
-const Controls = ({ currentView, currentTimezone, onViewSelect, onTimezoneSelect }) => (
-  <Menu mode="horizontal">
-    <Menu.Item>
-      <OptionPicker onChange={onViewSelect} defaultValue={currentView} options={VIEW_MODES} />
-    </Menu.Item>
-    <Menu.Item>
-      <OptionPicker onChange={onTimezoneSelect} defaultValue={currentTimezone} options={TIMEZONE} />
-    </Menu.Item>
-    <Menu.Item>
-      <Button type="primary">Something else</Button>
-    </Menu.Item>
-    <Menu.Item>
-      <Button>Something else</Button>
-    </Menu.Item>
-  </Menu>
-);
+// const _exportPdf = () => {
+//   html2canvas(document.querySelector('.ant-layout-content')).then((canvas) => {
+//     const imgData = canvas.toDataURL('image/png');
+//     // eslint-disable-next-line new-cap
+//     const pdf = new jsPDF();
+//     pdf.addImage(imgData, 'PNG', 0, 0);
+//     pdf.output('dataurlnewwindow');
+//     pdf.save('download.pdf');
+//   });
+// };
+
+const Controls = ({ currentView, currentTimezone, onViewSelect, onTimezoneSelect, onPrint }) => {
+  const PrintLogo = (
+    <PrinterOutlined style={{ fontSize: '1.8rem', verticalAlign: 'bottom', marginRight: 0 }} />
+  );
+  return (
+    <Menu mode="horizontal">
+      <Menu.Item>
+        <OptionPicker onChange={onViewSelect} defaultValue={currentView} options={VIEW_MODES} />
+      </Menu.Item>
+      <Menu.Item>
+        <OptionPicker
+          onChange={onTimezoneSelect}
+          defaultValue={currentTimezone}
+          options={TIMEZONE}
+        />
+      </Menu.Item>
+      <Menu.Item>
+        <Button>Print</Button>
+      </Menu.Item>
+      <Menu.Item>
+        <Button icon={PrintLogo} onClick={onPrint}>
+          Print
+        </Button>
+      </Menu.Item>
+    </Menu>
+  );
+};
 
 const mapStateToProps = ({ currentView, currentTimezone }) => ({
   currentView,
@@ -34,4 +60,5 @@ const mapStateToProps = ({ currentView, currentTimezone }) => ({
 export default connect(mapStateToProps, {
   onViewSelect: onViewModeChange,
   onTimezoneSelect: onTimezoneChange,
+  onPrint: print,
 })(Controls);
