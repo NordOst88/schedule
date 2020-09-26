3a618fe1340c7048468d3bdc6c64157630f1ee61
Stacey <stacey.sych@gmail.com>
2020-09-26 17:50:45 +0300

    feat: add no feedback text

--- src/constants/modalInfoConstants.js
+++ src/constants/modalInfoConstants.js
@@ -3,6 +3,7 @@ const STUDENT_ADD_FEEDBACK_TEXT = 'Add feedback';
 const INPUT_PLACEHOLDER = 'Write your feedback of the task;';
 const MENTOR_MODAL_TITLE = 'All feedbacks';
 const STUDENT_MODAL_TITLE = 'Feedback';
+const NO_FEEDBACK_TEXT = 'There have not been any feedbacks yet';
 
 export {
   MENTOR_SHOW_FEEDBACKS_TEXT,
@@ -10,4 +11,5 @@ export {
   INPUT_PLACEHOLDER,
   MENTOR_MODAL_TITLE,
   STUDENT_MODAL_TITLE,
+  NO_FEEDBACK_TEXT,
 };
