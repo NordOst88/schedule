ec7c3d53f689a64d6b051888bdd7607bd5b720f0
NordOst88 <NordOst88@users.noreply.github.com>
2020-09-27 00:54:42 +0400

    docs: add documentation comments to utilities for table

--- src/utils/getAvatarSrc.js
+++ src/utils/getAvatarSrc.js
@@ -1,5 +1,10 @@
 import logoRSSchool from '../assets/images/logo_rs.svg';
 
+/**
+ * Get organizer avatar's URL from github.
+ * @param {string} url - URL of organizer's page.
+ * @returns {(string|null)} Organizer avatar's URL or default logo URL or null.
+ */
 function getAvatarSrc(url) {
   if (url) {
     return url.indexOf('github') > -1 ? `${url}.png?size=48` : logoRSSchool;
